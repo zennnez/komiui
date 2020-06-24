43bbe472378be6d5e0a0cd76c8276af888e256cb
Sudeep Duhoon <rishi.gothic@gmail.com>
2020-06-24 21:21:35 +0530

    Revert "mm: Boost when memory pressure becomes high"
    This reverts commit e283b3c61dc890b5b27d524fc37390bcf75425cc.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -69,8 +69,6 @@
 #include <linux/lockdep.h>
 #include <linux/nmi.h>
 #include <linux/psi.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
@@ -4170,7 +4168,6 @@ retry:
 
 	/* Boost when memory is low so allocation latency doesn't get too bad */
 	devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 100);
-	cpu_input_boost_kick_max(100);
 	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
 
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
